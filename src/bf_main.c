16aacf06d1b635f3f801efcc0ed548b6d82ab337
Brian Rieder <brieder777@users.noreply.github.com>
2015-01-23 11:23:29 -0500

    Added calls to parsers and flag error

--- src/bf_main.c
+++ src/bf_main.c
@@ -11,12 +11,26 @@ int main(int argc, char * * argv)
 	// Prompt user for input:
 	char * user_input = calloc(1024, sizeof(char));
 	printf("Input: ");
-	scanf("%s", &user_input);
+	// scanf("%s", &user_input);
+	int input_itr = 0;
+	char ch = getchar();
+	while(ch != '\n' && input_itr < 1024) {
+		user_input[input_itr] = ch;
+		ch = getchar();
+		++input_itr;
+	}
+	user_input[input_itr] = '\0';
 	// Create the file parser
 	BFFile * bfptr = BFFile_constructor(&input_file, &user_input, &memptr_arr);
-
-
-
+	// Begin parsing the file
+	if(ascii_flag)
+		parseBFFile_ascii(bfptr);
+	else if(integer_flag)
+		parseBFFile_integer(bfptr);
+	else
+		FATAL("Flag error");
+	// Exit message upon completion
+	printf("Execution complete.\n");
 	return EXIT_SUCCESS;
 }
 
