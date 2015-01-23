6854e3a13a91fd652b50320ca13b7104d8b9744d
Brian Rieder <brieder777@users.noreply.github.com>
2015-01-23 14:01:56 -0500

    Added looping functionality

--- src/bf_fileparse.c
+++ src/bf_fileparse.c
@@ -5,8 +5,9 @@ BFFile * BFFile_constructor(FILE * * file_ptr, char * * user_input, int * * memp
 	BFFile * bfptr = malloc(sizeof(BFFile));
 	bfptr->file_ptr = *file_ptr;
 	bfptr->val_at_fileptr = '0';
+	bfptr->val_at_inputind = '0';
 	bfptr->curr_mem_index = 0;
-	bfptr->curr_inp_index = 0;
+	bfptr->curr_inp_index = -1;
 	bfptr->memptr_arr = *memptr_arr;
 	bfptr->user_input = *user_input;
 	return bfptr;
@@ -40,11 +41,11 @@ void parseBFFile_ascii(BFFile * bfptr)
 			case DECVAL_CMD:
 				decvalHandler(bfptr);
 				break;
-			case STLOOP_CMD:
-				startloopHandler(bfptr);
-				break;
 			case ENDLOOP_CMD:
 				endloopHandler(bfptr);
+				break;
+			case STLOOP_CMD:
+				startloopHandler(bfptr);
 				break;
 			case READCH_CMD:
 				readchHandler(bfptr);
@@ -52,8 +53,6 @@ void parseBFFile_ascii(BFFile * bfptr)
 			case PRINTCH_CMD:
 				printchHandler(bfptr);
 				break;
-			default:
-				continue;
 		}
 		// Iterate to next character in the file
 		bfptr->val_at_fileptr = fgetc(bfptr->file_ptr);
@@ -69,14 +68,14 @@ void parseBFFile_integer(BFFile * bfptr)
 void incptrHandler(BFFile * bfptr)
 {
 	if(bfptr->curr_mem_index == 1023)
-		FATAL("Overextended memory bounds.")
+		FATAL("Overextended memory bounds -- aborting.")
 	bfptr->curr_mem_index += 1;
 }
 
 void decptrHandler(BFFile * bfptr)
 {
 	if(bfptr->curr_mem_index == 0)
-		FATAL("Cannot access memory below cell zero.")
+		FATAL("Cannot access memory below cell zero -- aborting.")
 	bfptr->curr_mem_index -= 1;
 }
 
@@ -92,17 +91,60 @@ void decvalHandler(BFFile * bfptr)
 
 void startloopHandler(BFFile * bfptr)
 {
-
+	int cmd_count = 1;
+	int * iterator = &(bfptr->memptr_arr[bfptr->curr_mem_index]);
+	bfptr->val_at_fileptr = fgetc(bfptr->file_ptr);
+	while(*iterator > 0)
+	{
+		while(bfptr->val_at_fileptr != ENDLOOP_CMD) {
+			switch(bfptr->val_at_fileptr) {
+				case INCPTR_CMD:
+					incptrHandler(bfptr);
+					break;
+				case DECPTR_CMD:
+					decptrHandler(bfptr);
+					break;
+				case INCVAL_CMD:
+					incvalHandler(bfptr);
+					break;
+				case DECVAL_CMD:
+					decvalHandler(bfptr);
+					break;
+				case STLOOP_CMD:
+					startloopHandler(bfptr);
+					break;
+				case READCH_CMD:
+					readchHandler(bfptr);
+					break;
+				case PRINTCH_CMD:
+					printchHandler(bfptr);
+					break;
+				case EOF:
+					FATAL("No loop terminator found before EOF -- aborting.")
+					break;
+			}
+			++cmd_count;
+			bfptr->val_at_fileptr = fgetc(bfptr->file_ptr);
+		}
+		iterator = &(bfptr->memptr_arr[bfptr->curr_mem_index]);
+		if(*iterator != 0) {
+			fseek(bfptr->file_ptr, (-1) * cmd_count, SEEK_CUR);
+			cmd_count = 1;
+		}
+		bfptr->val_at_fileptr = fgetc(bfptr->file_ptr);
+	}
 }
 
 void endloopHandler(BFFile * bfptr)
 {
-
+	FATAL("No initiated loop to terminate -- aborting.");
 }
 
 void readchHandler(BFFile * bfptr)
 {
-	
+	++bfptr->curr_inp_index;
+	bfptr->val_at_inputind = bfptr->user_input[curr_inp_index];
+	bfptr->memptr_arr[curr_mem_index] = bfptr->val_at_inputind;
 }
 
 void printchHandler(BFFile * bfptr)
